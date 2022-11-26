1210843f362532fbc85fe6a12f3a65d578b8aa59
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-11-26 19:36:31 +0700

    selesai desain check list

--- src/component/componentContainerTodo/inputGroup.jsx
+++ src/component/componentContainerTodo/inputGroup.jsx
@@ -1,6 +1,24 @@
 
 
+// COMPONENT LIST TODO
+const ListTodo = ()=>{
+    return(
+        <div className="list-item">
+            <div className="check-list">
+                    <input type="checkbox" name="" id="checkList" />
+                    <label htmlFor="checkList" className="circle-list">
+                        <img src="./assets/images/icon-check.svg" alt="" />
+                    </label>
+            </div>
+            <div className="todo-desc">
 
+            </div>
+            <button className="delete-todo">
+
+            </button>
+        </div>
+    )
+}
 
 // component input group
 
@@ -11,10 +29,10 @@ const InputGroup = ()=>{
            <div className="input-check">
                <div></div>
            </div>
-           <input type="search" placeHolder="Create a new Todo...." className="input-todo" />
+           <input type="search" placeholder="Create a new Todo...." className="input-todo" />
         </section>
         <section className="todo-list-container">
-            
+           <ListTodo/>
         </section>
         </>
     )
