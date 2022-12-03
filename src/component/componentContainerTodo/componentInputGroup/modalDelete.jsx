c19c93d05681de585358b7de6181cbd34180d342
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-12-04 01:43:17 +0700

    mulai fitur modalDeleteTodo all

--- src/component/componentContainerTodo/componentInputGroup/modalDelete.jsx
+++ src/component/componentContainerTodo/componentInputGroup/modalDelete.jsx
@@ -1,11 +1,11 @@
 // COMPONENT MODEL DELETE
-const ModalDelete =({dataIdModal,eventDelete})=>{
+const ModalDelete =({dataIdModal,eventDelete,textModalBody})=>{
     return (
        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
        <div className="modal-dialog bg-transparent">
            <div className="modal-content modal-delete">
            <div className="modal-body text-center ">
-               apakah anda yakin ingin menghapusnya?
+               {textModalBody}
            </div>
            <div className="modal-footer">
                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
