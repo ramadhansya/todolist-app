6211231b933a27c92bf0fe3ab7e5487242f3e34a
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-12-03 10:18:42 +0700

    fix bugs

--- src/component/componentContainerTodo/inputGroup.jsx
+++ src/component/componentContainerTodo/inputGroup.jsx
@@ -61,12 +61,12 @@ const AlertUndefinedComponent = ({refElement})=>{
         <>
         <div className="toast-container position-fixed top-0 end-0 p-3 ">
         
-        <div className="toast  fade   text-bg-primary hide " ref={refElement} role="alert" aria-live="assertive" aria-atomic="true">
+        <div className="toast  fade   text-bg-danger hide " ref={refElement} role="alert" aria-live="assertive" aria-atomic="true">
   <div className="d-flex">
     <div className="toast-body fs-6">
     todo tidak boleh kosong
     </div>
-    <button type="button" className="btn-close me-2 m-auto bg-light" data-bs-dismiss="toast" aria-label="Close"></button>
+    <button type="button" className="btn-close me-2 m-auto navbar-dark" data-bs-dismiss="toast" aria-label="Close"></button>
   </div>
 </div>
         </div>
