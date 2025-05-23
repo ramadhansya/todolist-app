468f608c6814480daca2e4e766c3e477e149f5dc
Syahrul <ramadhans078@gmail.com>
2025-05-23 10:01:57 +0700

    Update listTodo.jsx

--- src/component/componentContainerTodo/componentInputGroup/listTodo.jsx
+++ src/component/componentContainerTodo/componentInputGroup/listTodo.jsx
@@ -1,24 +1,49 @@
+const ListTodo = ({ todo, dataBtnId, changeModal, eventCompleted, openUpdateModal }) => {
+  return (
+    <div className="list-item">
+      <div className="check-list">
+        <button className="circle-list">
+          <img
+            src="./assets/images/icon-check.svg"
+            className="img-checking"
+            onClick={eventCompleted}
+            alt=""
+          />
+        </button>
+      </div>
+      <div className="todo-desc">
+        <span className="todo-text">{todo}</span>
+      </div>
 
+      {/* Tombol hapus */}
+      <button
+        className="delete-todo"
+        style={{ display: dataBtnId == 0 ? 'none' : 'initial' }}
+      >
+        <img
+          src="./assets/images/icon-cross.svg"
+          data-bs-toggle="modal"
+          data-id={dataBtnId}
+          data-bs-target="#modalDeleteItem"
+          className="imgDeleteTodo"
+          alt=""
+          onClick={changeModal}
+        />
+      </button>
 
-
-// COMPONENT LIST TODO
-const ListTodo = ({todo,dataBtnId,changeModal,eventCompleted})=>{
-    return(
-        <div className="list-item">
-            <div className="check-list">
-                    <button className="circle-list" >
-                        <img src="./assets/images/icon-check.svg" className="img-checking" onClick={eventCompleted} alt="" />
-                    </button>
-            </div>
-            <div className="todo-desc">
-              <span className="todo-text">{todo}</span>
-            </div>
-            <button className="delete-todo" style={{display:(dataBtnId == 0) ? 'none' : 'initial'}} >
-             <img src="./assets/images/icon-cross.svg"  data-bs-toggle="modal" data-id={dataBtnId} data-bs-target="#modalDeleteItem"  className="imgDeleteTodo" alt="" onClick={changeModal} />
-            </button>
-        </div>
-        
-    )
-}
+      {/* Tombol update */}
+      {dataBtnId != 0 && (
+        <button
+          className="edit-todo btn btn-warning ms-2"
+          data-bs-toggle="modal"
+          data-bs-target="#modalUpdateItem"
+          onClick={() => openUpdateModal(dataBtnId)}
+        >
+          Edit
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default ListTodo;
