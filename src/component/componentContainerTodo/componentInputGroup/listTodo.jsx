93360eca6df1df4c26753abd79ed7f7907e7ed5f
Syahrul Ramadhan <ramadhans078@gmail.com>
2025-05-23 13:43:51 +0700

    Update listtodo

--- src/component/componentContainerTodo/componentInputGroup/listTodo.jsx
+++ src/component/componentContainerTodo/componentInputGroup/listTodo.jsx
@@ -1,39 +1,48 @@
 // COMPONENT LIST TODO
 const ListTodo = ({ todo, dataBtnId, changeModal, eventCompleted, openModalUpdate }) => {
-    return (
-        <div className="list-item">
-            <div className="check-list">
-                <button className="circle-list">
-                    <img src="./assets/images/icon-check.svg" className="img-checking" onClick={eventCompleted} alt="" />
-                </button>
-            </div>
-            <div className="todo-desc">
-                <span className="todo-text">{todo}</span>
-            </div>
-            <div className="todo-actions">
-                <button
-                    className="btn btn-sm btn-warning me-1"
-                    data-bs-toggle="modal"
-                    data-bs-target="#modalUpdateItem"
-                    onClick={() => openModalUpdate(dataBtnId)}
-                    style={{ display: dataBtnId == 0 ? 'none' : 'inline-block' }}
-                >
-                    Edit
-                </button>
-                <button className="delete-todo" style={{ display: dataBtnId == 0 ? 'none' : 'initial' }}>
-                    <img
-                        src="./assets/images/icon-cross.svg"
-                        data-bs-toggle="modal"
-                        data-id={dataBtnId}
-                        data-bs-target="#modalDeleteItem"
-                        className="imgDeleteTodo"
-                        alt=""
-                        onClick={changeModal}
-                    />
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="list-item">
+      <div className="check-list">
+        <button className="circle-list">
+          <img
+            src="./assets/images/icon-check.svg"
+            className="img-checking"
+            onClick={eventCompleted}
+            alt=""
+          />
+        </button>
+      </div>
+      <div className="todo-desc">
+        <span className="todo-text">{todo}</span>
+      </div>
+      <div className="todo-actions">
+        {/* Tombol Edit: trigger modal lewat fungsi, bukan data-bs-atribut */}
+        <button
+          className="btn btn-sm btn-warning me-1"
+          onClick={() => openModalUpdate(dataBtnId)}
+          style={{ display: dataBtnId === 0 ? 'none' : 'inline-block' }}
+        >
+          Edit
+        </button>
+
+        {/* Tombol Delete: jika pakai modal lain (misal modal delete) tetap pakai data-bs-target */}
+        <button
+          className="delete-todo"
+          style={{ display: dataBtnId === 0 ? 'none' : 'initial' }}
+        >
+          <img
+            src="./assets/images/icon-cross.svg"
+            data-bs-toggle="modal"
+            data-id={dataBtnId}
+            data-bs-target="#modalDeleteItem"
+            className="imgDeleteTodo"
+            alt=""
+            onClick={changeModal}
+          />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ListTodo;
